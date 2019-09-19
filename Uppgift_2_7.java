520abef41d1f16c048a6d46cbe7977718b7f105c
Alexander Holmström <alexander.holmstro3@learnet.se>
2019-09-19 10:57:58 +0200

    Uppgift2.7

---
+++ Uppgift_2_7.java
@@ -0,0 +1,16 @@
+/*
+Övning 2.7 i boken
+2019-09-19
+Programmerare Alexander Holmström
+*/
+import javax.swing.*;
+public class Uppgift_2_7{
+	public static void main (String [] args){
+		char c1='\u00D8';
+		char c2='\u00F8';
+		char c3='\u03c0';
+		JOptionPane.showMessageDialog(null, ""+c1);
+		JOptionPane.showMessageDialog(null, ""+c2);
+		JOptionPane.showMessageDialog(null, ""+c3);
+	}
+}
